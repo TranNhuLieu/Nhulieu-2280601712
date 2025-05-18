d7b8a0573c2cf859362e1780098753edaa741f8c
trannhulieu <khaly121981@gmail.com>
2025-05-18 21:47:28 +0700

    b1

---
+++ ex01/ex01_01.py
@@ -0,0 +1,5 @@
+# Nhập tên và tuổi từ người dùng
+ten = input ("Nhập tên của bạn:")
+tuoi = input ("nhap tuổi của bạn:")
+# In thông điệp chào mừng với thông tin vừa nhập
+print("Chào mừng,", ten, "! Bạn", tuoi, "tuổi.")
---
+++ ex01/ex01_02.py
@@ -0,0 +1,6 @@
+#nhập bán kính người dùng
+ban_kinh = float(input("Nhập bán kính của hình tròn:"))
+#Tính diện tích hình tròn
+dien_tich = 3.14 * (ban_kinh ** 2)
+#In diện tích hình tròn ra màng hình
+print("Diện tích của hionhf tròn là:", dien_tich)
